b94cfc43e7aac50fafbf3d5c32f32a42f9f241cb
sye-9 <sayed.a-1@outlook.com>
2023-07-30 16:50:49 +0100

    messages selected/stored in array

---
+++ script.js
@@ -0,0 +1,11 @@
+
+// Storing messages
+const message1 = document.querySelector(".message1");
+const message2 = document.querySelector(".message2");
+const message3 = document.querySelector(".message3");
+const message4 = document.querySelector(".message4");
+const message5 = document.querySelector(".message5");
+
+const messageArray = [message1, message2, message3, message4, message5]; 
+
+
