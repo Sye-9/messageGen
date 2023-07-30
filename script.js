09eab81daf1fe585e64fb876a210691758cf1342
sye-9 <sayed.a-1@outlook.com>
2023-07-30 16:57:43 +0100

    message styling

--- script.js
+++ script.js
@@ -7,5 +7,3 @@ const message4 = document.querySelector(".message4");
 const message5 = document.querySelector(".message5");
 
 const messageArray = [message1, message2, message3, message4, message5]; 
-
-
