201ffd27cdf5196390b36f7c1127f9efbdd2d2e7
sye-9 <sayed.a-1@outlook.com>
2023-07-31 00:44:34 +0100

    message storage update (object)

--- script.js
+++ script.js
@@ -1,23 +1,40 @@
 
-// Storing messages
-const message1 = document.querySelector(".message1");
-const message2 = document.querySelector(".message2");
-const message3 = document.querySelector(".message3");
-const message4 = document.querySelector(".message4");
-const message5 = document.querySelector(".message5");
+// Messsage Storage
 
-const messageArray = [message1, message2, message3, message4, message5]; 
+const greeting1 = 'Hi';
+const greeting2 = 'Hello';
+const greeting3 = 'Hey';
+
+const message1 = 'Take a deep breath';
+const message2 = 'look on the Brightside';
+const message3 = 'Smile';
+
+const messFinal1 = 'Today is your day';
+const messFinal2 = 'Your happiness matters';
+const messFinal3 = 'You will be OK :)';
+
+
+let message = {
+    messageArray1: [greeting1, greeting2, greeting3],
+    messageArray2: [message1, message2, message3],
+    messageArray3: [messFinal1, messFinal2, messFinal3]
+}
+
+// console.log(messageArray.messageArray1[0])
 
 
 
 const randomMessage = () => {
-    return Math.floor(Math.random() * messageArray.length);
-    console.log("messageArray[randomMessage]");
-}
+    return Math.floor(Math.random() * message.length);
+};
+
+
 
 
 
 
 // Tasks
-// select message from array randomly (Done)
+// select message from array randomly (Done) !!!
 // add event listener to display random message once selected
+
+
