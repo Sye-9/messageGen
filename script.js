f69cdd8c46cf4e631ad8c356e01f28051b8783af
sye-9 <sayed.a-1@outlook.com>
2023-08-04 02:41:07 +0100

    object literals

--- script.js
+++ script.js
@@ -1,40 +1,127 @@
-
-// Messsage Storage
-
 const greeting1 = 'Hi';
 const greeting2 = 'Hello';
-const greeting3 = 'Hey';
-
-const message1 = 'Take a deep breath';
-const message2 = 'look on the Brightside';
-const message3 = 'Smile';
-
-const messFinal1 = 'Today is your day';
-const messFinal2 = 'Your happiness matters';
-const messFinal3 = 'You will be OK :)';
+const greeting3 = 'Welcome';
+const greeting4 = 'WatsUp';
+const greeting5 = 'Howdy';
+const greeting6 = 'Good morning';
+const greeting7 = 'Hey there';
 
 
-let message = {
-    messageArray1: [greeting1, greeting2, greeting3],
-    messageArray2: [message1, message2, message3],
-    messageArray3: [messFinal1, messFinal2, messFinal3]
+const middle1 = 'I am Ziolet';
+const middle2 = 'My Name is Alexander';
+const middle3 = 'Thank you for talking to me today';
+const middle4 = 'I have never seen you before';
+
+const end1 = 'You look Lovely today';
+const end2 = 'Your smile is contagious';
+const end3 = 'Happy Holidays';
+const end4 = 'Is today not a lovely day??';
+const end5 = 'How are you?';
+const end6 = 'It lovely meeting you today';
+
+
+let messageStorage = {
+    greet: [greeting1, greeting2, greeting3, greeting4, greeting5, greeting6, greeting7],
+    middle: [middle1, middle2, middle3,  middle4],
+    end: [end1, end2, end3, end4, end5, end6]
 }
 
-// console.log(messageArray.messageArray1[0])
+
+function greetGen (type) {
+
+    var greet = {
+      1 : function () {
+        return messageStorage.greet[0];
+      },
+      2 : function () {
+        return messageStorage.greet[1];
+      },
+      3 : function () {
+        return messageStorage.greet[2];
+      },
+      4 : function () {
+        return messageStorage.greet[3];
+      },
+      5 : function () {
+        return messageStorage.greet[4];
+      },
+      6 : function () {
+        return messageStorage.greet[5];
+      },
+      7 : function () {
+        return messageStorage.greet[6];
+      }
+    };
+    return greet[type]();
+  }
+
+var greetGenFunction = () => {
+  return greetGen(Math.floor(Math.random() * 7) + 1)
+}
+
+// console.log('----------------------------------------------------------------')
+
+function middleGen (type) {
+
+    var middle = {
+      1 : function () {
+        return messageStorage.middle[0];
+      },
+      2 : function () {
+        return messageStorage.middle[1];
+      },
+      3 : function () {
+        return messageStorage.middle[2];
+      },
+      4 : function () {
+        return messageStorage.middle[3];
+      }
+    };
+    return middle[type]();
+  }
+
+var middleGenFunction = () => {
+  return middleGen(Math.floor(Math.random() * 4) + 1)
+}
 
 
+// console.log('----------------------------------------------------------------')
 
-const randomMessage = () => {
-    return Math.floor(Math.random() * message.length);
-};
+function endGen (type) {
+
+    var end = {
+      1 : function () {
+        return messageStorage.end[0];
+      },
+      2 : function () {
+        return messageStorage.end[1];
+      },
+      3 : function () {
+        return messageStorage.end[2];
+      },
+      4 : function () {
+        return messageStorage.end[3];
+      },
+      5 : function () {
+        return messageStorage.end[4];
+      },
+      6 : function () {
+        return messageStorage.end[5];
+      }
+    };
+    return end[type]();
+  }
+
+var endGenFunction = () => {
+  return endGen(Math.floor(Math.random() * 6) + 1)
+}
 
 
+// console.log('----------------------------------------------------------------')
 
 
+let wholeMessage = () => {
+    return `${greetGenFunction()}, ${middleGenFunction()}, ${endGenFunction()}`
+}
 
-
-// Tasks
-// select message from array randomly (Done) !!!
-// add event listener to display random message once selected
-
-
+console.log(wholeMessage())
